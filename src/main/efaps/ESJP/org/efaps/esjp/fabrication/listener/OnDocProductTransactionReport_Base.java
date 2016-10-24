9452512c4053172c6be66ae682152a37267b6b38
Jan Moxter <jan.moxter@innobix.com>
2016-10-24 18:08:18 -0500

    - Issue eFaps/eFapsApp-Sales#330: Update DocProductTransactionReport to use DataBean

--- src/main/efaps/ESJP/org/efaps/esjp/fabrication/listener/OnDocProductTransactionReport_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/fabrication/listener/OnDocProductTransactionReport_Base.java
@@ -43,9 +43,12 @@ import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.db.InstanceUtils;
 import org.efaps.esjp.products.Product;
 import org.efaps.esjp.sales.listener.IOnDocProductTransactionReport;
+import org.efaps.esjp.sales.report.DocProductTransactionReport_Base.DataBean;
 import org.efaps.esjp.sales.report.DocProductTransactionReport_Base.DynDocProductTransactionReport;
 import org.efaps.esjp.sales.util.Sales;
 import org.efaps.util.EFapsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The Class OnDocProductTransactionReport_Base.
@@ -57,136 +60,163 @@ import org.efaps.util.EFapsException;
 public abstract class OnDocProductTransactionReport_Base
     implements IOnDocProductTransactionReport
 {
+    /**
+     * Logging instance used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(OnDocProductTransactionReport.class);
+
+    /**
+     * Analyze bean.
+     *
+     * @param _parameter Parameter as passed by the eFaps API
+     * @param _dynReport the dyn report
+     * @param _bean the bean
+     * @return the collection< data bean>
+     * @throws EFapsException on error
+     */
+    protected Collection<DataBean> analyzeBean(final Parameter _parameter,
+                                               final DynDocProductTransactionReport _dynReport,
+                                               final DataBean _bean)
+        throws EFapsException
+    {
+        LOG.debug("Analysing : {}", _bean);
+        final Collection<DataBean> ret = new ArrayList<>();
+
+        BigDecimal produced = BigDecimal.ZERO;
+        final Set<Instance> prInsts = new HashSet<>();
+
+        // get the produced quantity
+        final boolean isIndividual = (InstanceUtils.isKindOf(_bean.getProductInst(),
+                        CIProducts.ProductIndividualAbstract));
+
+        final QueryBuilder pqQueryBldr = new QueryBuilder(CISales.ProductionReport);
+        pqQueryBldr.addWhereAttrNotEqValue(CISales.ProductionReport.Status, Status.find(
+                        CISales.ProductionReportStatus.Canceled));
+
+        final QueryBuilder pqransQueryBldr = new QueryBuilder(isIndividual ? CIProducts.TransactionIndividualInbound
+                        : CIProducts.TransactionInbound);
+        pqransQueryBldr.addWhereAttrEqValue(CIProducts.TransactionAbstract.Product, _bean.getProductInst());
+        pqransQueryBldr.addWhereAttrInQuery(CIProducts.TransactionAbstract.Document, pqQueryBldr.getAttributeQuery(
+                        CISales.ProductionReport.ID));
+        final MultiPrintQuery pqmulti = pqransQueryBldr.getCachedPrint4Request();
+        final SelectBuilder selPRInst = SelectBuilder.get().linkto(CIProducts.TransactionAbstract.Document).instance();
+        pqmulti.addSelect(selPRInst);
+        pqmulti.addAttribute(CIProducts.TransactionAbstract.Quantity);
+        pqmulti.executeWithoutAccessCheck();
+        while (pqmulti.next()) {
+            produced = produced.add(pqmulti.<BigDecimal>getAttribute(CIProducts.TransactionAbstract.Quantity));
+            prInsts.add(pqmulti.getSelect(selPRInst));
+        }
+        if (prInsts.isEmpty()) {
+            ret.add(_bean);
+        } else {
+            // get a factor for moved/produced
+            final BigDecimal factor = BigDecimal.ZERO.compareTo(produced) == 0
+                            ? BigDecimal.ONE
+                            : _bean.getQuantity().setScale(12, RoundingMode.HALF_UP)
+                                            .divide(produced, RoundingMode.HALF_UP);
+            final QueryBuilder pro2repQueryBldr = new QueryBuilder(CIFabrication.Process2ProductionReport);
+            pro2repQueryBldr.addWhereAttrEqValue(CIFabrication.Process2ProductionReport.ToLink, prInsts.toArray());
+
+            final QueryBuilder pro2UsageQueryBldr = new QueryBuilder(CIFabrication.Process2UsageReport);
+            pro2UsageQueryBldr.addWhereAttrInQuery(CIFabrication.Process2UsageReport.FromLink,
+                            pro2repQueryBldr.getAttributeQuery(CIFabrication.Process2UsageReport.FromLink));
+
+            final QueryBuilder usageTransQueryBldr = new QueryBuilder(CIProducts.TransactionOutbound);
+            usageTransQueryBldr.addType(CIProducts.TransactionIndividualOutbound);
+            usageTransQueryBldr.addWhereAttrInQuery(CIProducts.TransactionOutbound.Document,
+                            pro2UsageQueryBldr.getAttributeQuery(CIFabrication.Process2UsageReport.ToLink));
+
+            final MultiPrintQuery multi = usageTransQueryBldr.getCachedPrint4Request();
+
+            final SelectBuilder selProduct = SelectBuilder.get().linkto(
+                            CIProducts.TransactionAbstract.Product);
+            final SelectBuilder selProductInst = new SelectBuilder(selProduct).instance();
+            final SelectBuilder selProductName = new SelectBuilder(selProduct).attribute(
+                            CIProducts.ProductAbstract.Name);
+            final SelectBuilder selProductDescr = new SelectBuilder(selProduct).attribute(
+                            CIProducts.ProductAbstract.Description);
+            multi.addSelect(selProductInst, selProductName, selProductDescr);
+            multi.addAttribute(CIProducts.TransactionOutbound.Quantity);
+            multi.executeWithoutAccessCheck();
+            final Map<Instance, DataBean> added = new HashMap<>();
+            final Set<Instance> duplicated = new HashSet<>();
+            while (multi.next()) {
+                final Instance productInst = multi.getSelect(selProductInst);
+                final BigDecimal quantity = multi.getAttribute(CIProducts.TransactionAbstract.Quantity);
+
+                final DataBean dataBean;
+                if (added.containsKey(productInst)) {
+                    dataBean = added.get(productInst);
+                } else {
+                    final String productName = multi.getSelect(selProductName);
+                    final String productDescr = multi.getSelect(selProductDescr);
+                    dataBean = _dynReport.getBean(_parameter)
+                                    .setUoM(_bean.getUoM())
+                                    .setProduct(productName)
+                                    .setProductDescr(productDescr)
+                                    .setProductInst(productInst)
+                                    .setQuantity(BigDecimal.ZERO)
+                                    .setDocContact(_bean.getDocContact())
+                                    .setDocName(_bean.getDocName())
+                                    .setPartial(_bean.getPartial());
+                }
+                dataBean.setQuantity(dataBean.getQuantity().add(quantity));
+                added.put(productInst, dataBean);
+
+                if (multi.getCurrentInstance().getType().isCIType(
+                                CIProducts.TransactionIndividualOutbound)) {
+                    duplicated.add(new Product().getProduct4Individual(_parameter, productInst));
+                }
+            }
+            for (final Entry<Instance, DataBean> entry : added.entrySet()) {
+                if (!duplicated.contains(entry.getKey()) && !entry.getKey().equals(_bean.getProductInst())) {
+                    final BigDecimal newQuantity = entry.getValue().getQuantity().multiply(factor);
+                    if (newQuantity.compareTo(BigDecimal.ZERO) > 0) {
+                        entry.getValue().setQuantity(newQuantity);
+                        ret.add(entry.getValue());
+                    }
+                }
+            }
+        }
+        LOG.debug("returned : {}", ret);
+        return ret;
+    }
 
     @Override
     public void updateValues(final Parameter _parameter,
                              final DynDocProductTransactionReport _dynReport,
-                             final Collection<Map<String, ?>> _values)
+                             final Collection<DataBean> _values)
         throws EFapsException
     {
         if (Sales.REPORT_DOCPRODTRANS_FAB.get()) {
             final Map<String, Object> filter = _dynReport.getFilteredReport().getFilterMap(_parameter);
-            boolean hasInd = filter.containsKey("analyzeFabrication")
+            final boolean analyze = filter.containsKey("analyzeFabrication")
                             && BooleanUtils.isTrue((Boolean) filter.get("analyzeFabrication"));
-            final Set<Map<String, ?>> prevDup = new HashSet<>();
-            while (hasInd) {
-                final Iterator<Map<String, ?>> iter = _values.iterator();
-                hasInd = false;
-                final List<Map<String, ?>> newValues = new ArrayList<>();
-                while (iter.hasNext()) {
-                    final Map<String, ?> map = iter.next();
-                    final Instance prodInst = (Instance) map.get("productInst");
-                    final BigDecimal moved = (BigDecimal) map.get("quantity");
-                    if (InstanceUtils.isKindOf(prodInst, CIProducts.ProductIndividualAbstract)) {
-                        hasInd = true;
-                        BigDecimal produced = BigDecimal.ZERO;
-                        final Set<Instance> prInsts = new HashSet<>();
-                        // get the produced quantity
-                        final QueryBuilder pqQueryBldr = new QueryBuilder(CISales.ProductionReport);
-                        pqQueryBldr.addWhereAttrNotEqValue(CISales.ProductionReport.Status,
-                                        Status.find(CISales.ProductionReportStatus.Canceled));
 
-                        final QueryBuilder pqransQueryBldr = new QueryBuilder(CIProducts.TransactionIndividualInbound);
-                        pqransQueryBldr.addWhereAttrEqValue(CIProducts.TransactionIndividualInbound.Product, prodInst);
-                        pqransQueryBldr.addWhereAttrInQuery(CIProducts.TransactionIndividualInbound.Document,
-                                        pqQueryBldr.getAttributeQuery(CISales.ProductionReport.ID));
-                        final MultiPrintQuery pqmulti = pqransQueryBldr.getPrint();
-                        final SelectBuilder selPRInst = SelectBuilder.get()
-                                        .linkto(CIProducts.TransactionAbstract.Document)
-                                        .instance();
-                        pqmulti.addSelect(selPRInst);
-                        pqmulti.addAttribute(CIProducts.TransactionAbstract.Quantity);
-                        pqmulti.executeWithoutAccessCheck();
-                        while (pqmulti.next()) {
-                            produced = produced.add(pqmulti.<BigDecimal>getAttribute(
-                                            CIProducts.TransactionAbstract.Quantity));
-                            prInsts.add(pqmulti.getSelect(selPRInst));
-                        }
-                        if (prInsts.isEmpty()) {
-                            hasInd = false;
-                            // prevent duplicated adding due to looping
-                            if (!prevDup.contains(map)) {
-                                prevDup.add(map);
-                                newValues.add(map);
-                            }
+            if (analyze) {
+                boolean retry = true;
+                final List<DataBean> finalBeans  = new ArrayList<>();
+                final List<DataBean> loopBeans = new ArrayList<>();
+                while (retry) {
+                    final Iterator<DataBean> iter = loopBeans.isEmpty() ? _values.iterator() : loopBeans.iterator();
+                    LOG.debug("Iteration for {} elements.",loopBeans.isEmpty() ? _values.size() : loopBeans.size());
+                    final List<DataBean> beans = new ArrayList<>();
+                    while (iter.hasNext()) {
+                        final DataBean bean = iter.next();
+                        final Collection<DataBean> newBeans = analyzeBean(_parameter, _dynReport, bean);
+                        if (newBeans.size() == 1 && newBeans.iterator().next().equals(bean)) {
+                            finalBeans.add(bean);
                         } else {
-                            // get a factor for moved/produced
-                            final BigDecimal factor = BigDecimal.ZERO.compareTo(produced) == 0
-                                            ? BigDecimal.ONE
-                                            : moved.setScale(8, RoundingMode.HALF_UP)
-                                                            .divide(produced, RoundingMode.HALF_UP);
-
-                            final QueryBuilder pro2repQueryBldr = new QueryBuilder(
-                                            CIFabrication.Process2ProductionReport);
-                            pro2repQueryBldr.addWhereAttrEqValue(CIFabrication.Process2ProductionReport.ToLink,
-                                            prInsts.toArray());
-
-                            final QueryBuilder pro2UsageQueryBldr = new QueryBuilder(CIFabrication.Process2UsageReport);
-                            pro2UsageQueryBldr.addWhereAttrInQuery(CIFabrication.Process2UsageReport.FromLink,
-                                            pro2repQueryBldr.getAttributeQuery(
-                                                            CIFabrication.Process2UsageReport.FromLink));
-
-                            final QueryBuilder usageTransQueryBldr = new QueryBuilder(CIProducts.TransactionOutbound);
-                            usageTransQueryBldr.addType(CIProducts.TransactionIndividualOutbound);
-                            usageTransQueryBldr.addWhereAttrInQuery(CIProducts.TransactionOutbound.Document,
-                                            pro2UsageQueryBldr.getAttributeQuery(
-                                                            CIFabrication.Process2UsageReport.ToLink));
-
-                            final MultiPrintQuery multi = usageTransQueryBldr.getCachedPrint4Request();
-
-                            final SelectBuilder selProduct = SelectBuilder.get().linkto(
-                                            CIProducts.TransactionAbstract.Product);
-                            final SelectBuilder selProductInst = new SelectBuilder(selProduct).instance();
-                            final SelectBuilder selProductName = new SelectBuilder(selProduct).attribute(
-                                            CIProducts.ProductAbstract.Name);
-                            final SelectBuilder selProductDescr = new SelectBuilder(selProduct).attribute(
-                                            CIProducts.ProductAbstract.Description);
-                            multi.addSelect(selProductInst, selProductName, selProductDescr);
-                            multi.addAttribute(CIProducts.TransactionOutbound.Quantity);
-                            multi.executeWithoutAccessCheck();
-                            final Map<Instance, Map<String, Object>> added = new HashMap<>();
-                            final Set<Instance> duplicated = new HashSet<>();
-                            while (multi.next()) {
-                                final Instance productInst = multi.getSelect(selProductInst);
-                                final BigDecimal quantity = multi.getAttribute(CIProducts.TransactionAbstract.Quantity);
-
-                                final Map<String, Object> newMap;
-                                if (added.containsKey(productInst)) {
-                                    newMap = added.get(productInst);
-                                } else {
-                                    newMap = new HashMap<>();
-                                    newMap.putAll(map);
-                                    final String productName = multi.getSelect(selProductName);
-                                    final String productDescr = multi.getSelect(selProductDescr);
-                                    newMap.put("productInst", productInst);
-                                    newMap.put("product", productName);
-                                    newMap.put("productDescr", productDescr);
-                                    newMap.put("quantity", BigDecimal.ZERO);
-                                }
-                                newMap.put("quantity", ((BigDecimal) newMap.get("quantity")).add(quantity));
-                                added.put(productInst, newMap);
-
-                                if (multi.getCurrentInstance().getType().isCIType(
-                                                CIProducts.TransactionIndividualOutbound)) {
-                                    duplicated.add(new Product().getProduct4Individual(_parameter, productInst));
-                                }
-                            }
-                            iter.remove();
-
-                            for (final Entry<Instance, Map<String, Object>> entry : added.entrySet()) {
-                                if (!duplicated.contains(entry.getKey())) {
-                                    final Map<String, Object> lastMap = entry.getValue();
-                                    lastMap.put("quantity", ((BigDecimal) lastMap.get("quantity")).multiply(factor));
-                                    newValues.add(lastMap);
-                                }
-                            }
+                            beans.addAll(newBeans);
                         }
-                    } else {
-                        iter.remove();
-                        newValues.add(map);
                     }
+                    loopBeans.clear();
+                    loopBeans.addAll(beans);
+                    retry = !loopBeans.isEmpty();
                 }
-                _values.addAll(newValues);
+                _values.clear();
+                _values.addAll(finalBeans);
             }
         }
     }
